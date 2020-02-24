368c0369dfd0848fc475afe92cf3ac74aa2c77f9
Vlad2498 <vaio19ta@student.ju.se>
2020-02-24 11:00:07 +0100

    Adds Navigation Bar.

--- app/src/main/java/com/example/aristotle/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/aristotle/MainActivity/MainActivity.kt
@@ -1,13 +1,65 @@
 package com.example.aristotle.MainActivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.aristotle.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.aristotle.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val m0nNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        Fragmnent Switcher
+        when(item.itemId){
+            R.id.calendar -> {
+                println("calendar pressed")
+                replaceFragment(CalendarFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.newMeeting -> {
+                println("New Meeting pressed")
+                replaceFragment(MeetingFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.statistics -> {
+                println("Statistics pressed")
+                replaceFragment(StatisticsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.contacts -> {
+                println("Contacts pressed")
+                replaceFragment(ContactsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Setting the default view of the app
+        bottom_navigation.setOnNavigationItemSelectedListener(m0nNavigationItemSelectedListener)
+        replaceFragment(CalendarFragment())
+
+
+
     }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer , fragment)
+        fragmentTransaction.commit()
+    }
+
 }
