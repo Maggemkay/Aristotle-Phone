939fb9bd08548baccbe8170a2f79645b6269b3c5
Sophie <59923195+zaiiss@users.noreply.github.com>
2020-02-27 08:51:15 +0100

    Login view, forgot password, reset password
    Added front-end for Login view, forgot password, reset password
    
    Changed fragmented login with to activity main because I was not able to test it in the emulator.
    
    Also added material design in styles. Strings is also added.

--- app/src/main/java/com/example/aristotle/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/aristotle/MainActivity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.aristotle.MainActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.aristotle.R
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
