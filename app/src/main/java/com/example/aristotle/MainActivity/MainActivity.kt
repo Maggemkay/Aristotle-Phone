a7f2ebf02d99bcf7be3c84f6172b08cc830df84c
Marcus Gullstrand <guma1502@student.ju.se>
2020-03-17 16:00:08 +0100

    Adds transcription demo to contacts.

--- app/src/main/java/com/example/aristotle/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/aristotle/MainActivity/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             R.id.contacts -> {
-                replaceFragment(ContactsFragment())
+                replaceFragment(TranscriptionFragment())
                 return@OnNavigationItemSelectedListener true
             }
 
