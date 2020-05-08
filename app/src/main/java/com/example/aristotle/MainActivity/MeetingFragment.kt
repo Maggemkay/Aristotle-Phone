17f27b520447d6e1830fea9075a1c56f552f9231
maggemkay <magge.96@hotmail.com>
2020-05-08 16:59:28 +0200

    Adds code for new meeting.

--- app/src/main/java/com/example/aristotle/MainActivity/MeetingFragment.kt
+++ app/src/main/java/com/example/aristotle/MainActivity/MeetingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.aristotle
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,14 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_meeting.*
 import java.util.*
 
 class MeetingFragment : Fragment() {
 
-    private var selectedDate = Calendar.getInstance()
+    private var startTimeSelected = Calendar.getInstance()
+    private var endTimeSelected = Calendar.getInstance()
 
+    private lateinit var currentSelected : Calendar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,12 +33,24 @@ class MeetingFragment : Fragment() {
         timePicker.setIs24HourView(true)
         calendarViewSet.showCurrentMonthPage()
 
-        time.setOnClickListener {
+        startTime.setOnClickListener {
             val animation = AnimationUtils.loadAnimation(
                 activity, R.anim.slide_up)
             setter.visibility = View.VISIBLE
             setter.startAnimation(animation)
-            time.isEnabled = false
+            deactivateElements()
+
+            currentSelected = startTimeSelected
+        }
+
+        endTime.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(
+                activity, R.anim.slide_up)
+            setter.visibility = View.VISIBLE
+            setter.startAnimation(animation)
+            deactivateElements()
+
+            currentSelected = endTimeSelected
         }
 
         cancelTime.setOnClickListener {
@@ -46,8 +62,7 @@ class MeetingFragment : Fragment() {
             timePicker.hour = c.get(Calendar.HOUR_OF_DAY)
             timePicker.minute = c.get(Calendar.MINUTE)
             calendarViewSet.showCurrentMonthPage()
-
-            time.isEnabled = true
+            activateElements()
         }
 
         doneTime.setOnClickListener {
@@ -55,26 +70,58 @@ class MeetingFragment : Fragment() {
                 activity, R.anim.slide_down)
             setter.startAnimation(animation)
             setter.visibility = View.INVISIBLE
-            time.isEnabled = true
-            Toast.makeText(activity , "Selected time " + timePicker.hour + " : " + timePicker.minute , Toast.LENGTH_SHORT).show()
-            selectedDate.set(Calendar.HOUR, timePicker.hour)
-            selectedDate.set(Calendar.MINUTE, timePicker.minute)
 
-            Log.d("Selected year", selectedDate.get(Calendar.YEAR).toString())
-            Log.d("Selected month", selectedDate.get(Calendar.MONTH).toString())
-            Log.d("Selected day", selectedDate.get(Calendar.DAY_OF_MONTH).toString())
-            Log.d("Selected hour", selectedDate.get(Calendar.HOUR).toString())
-            Log.d("Selected minute", selectedDate.get(Calendar.MINUTE).toString())
+            Toast.makeText(activity , "Selected time " + timePicker.hour.toString().padStart(2, '0') + ":" + timePicker.minute.toString().padStart(2, '0') , Toast.LENGTH_SHORT).show()
+            currentSelected.set(Calendar.HOUR, timePicker.hour)
+            currentSelected.set(Calendar.MINUTE, timePicker.minute)
+
+            Log.d("Selected year", currentSelected.get(Calendar.YEAR).toString())
+            Log.d("Selected month", currentSelected.get(Calendar.MONTH).toString())
+            Log.d("Selected day", currentSelected.get(Calendar.DAY_OF_MONTH).toString())
+            Log.d("Selected hour", currentSelected.get(Calendar.HOUR_OF_DAY).toString())
+            Log.d("Selected minute", currentSelected.get(Calendar.MINUTE).toString())
+
+            activateElements()
         }
 
         calendarViewSet.setOnDayClickListener { eventDay ->
             val clickedDayCalendar: Calendar = eventDay.calendar
+            currentSelected.set(Calendar.YEAR, clickedDayCalendar.get(Calendar.YEAR))
+            currentSelected.set(Calendar.MONTH, clickedDayCalendar.get(Calendar.MONTH))
+            currentSelected.set(Calendar.DAY_OF_MONTH, clickedDayCalendar.get(Calendar.DAY_OF_MONTH))
+        }
 
-            selectedDate.set(Calendar.YEAR, clickedDayCalendar.get(Calendar.YEAR))
-            selectedDate.set(Calendar.MONTH, clickedDayCalendar.get(Calendar.MONTH))
-            selectedDate.set(Calendar.DAY_OF_MONTH, clickedDayCalendar.get(Calendar.DAY_OF_MONTH))
+        buttonNewMeetingBook.setOnClickListener {
+
+            // Save the meeting to file
+
+            val builder = this.context?.let { it1 -> AlertDialog.Builder(it1) }
+            builder?.setTitle("Your meeting is booked")
+            builder?.setMessage("Your meeting have been booked and added to the calendar.")
+            builder?.setPositiveButton("Ok") { dialog, which->
+
+                // Change fragment to calendar fragment
+
+            }
+            val dialog: AlertDialog = builder!!.create()
+            dialog.show()
         }
 
     }
+
+    private fun activateElements() {
+        startTime.isEnabled = true
+        endTime.isEnabled = true
+        textInputMeetingSubject.isEnabled = true
+        textInputLocation.isEnabled = true
+    }
+
+    private fun deactivateElements() {
+        startTime.isEnabled = false
+        endTime.isEnabled = false
+        textInputMeetingSubject.isEnabled = false
+        textInputLocation.isEnabled = false
+    }
+
 
 }
