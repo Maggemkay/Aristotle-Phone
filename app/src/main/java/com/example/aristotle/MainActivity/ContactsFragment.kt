7f8316a4db97d74525ce6eebb5482bc3afcf86e2
BullenBiss <mxpettersson@gmail.com>
2020-05-08 16:24:58 +0200

    Added save user function

--- app/src/main/java/com/example/aristotle/MainActivity/ContactsFragment.kt
+++ app/src/main/java/com/example/aristotle/MainActivity/ContactsFragment.kt
@@ -1,13 +1,17 @@
 package com.example.aristotle
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.aristotle.MainActivity.Adapters.UsersRecyclerViewAdapter
 import com.example.aristotle.Models.User
+import com.google.gson.Gson
+import java.io.File
+import java.io.FileWriter
+import java.io.Writer
 
 
 class ContactsFragment : Fragment() {
@@ -28,6 +32,7 @@ class ContactsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        saveUsers()
         return inflater.inflate(R.layout.fragment_viewcontact, container, false)
     }
 
@@ -40,6 +45,13 @@ class ContactsFragment : Fragment() {
         usersRecyclerView.hasFixedSize()
     }
 
+
+    private fun saveUsers(){
+        val user = User("1", "Bobus", "abc", "cool@email", "Bob", "Foo")
+        val writer: Writer = FileWriter(this.context?.filesDir?.path + "/Users.json" )
+        Gson().toJson(user, writer)
+        writer.close()
+    }
 
     private fun loadUsers() : List<User> {
         // Load users from local storage
