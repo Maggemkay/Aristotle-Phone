63815e52170db8721bc8d6d62d494d7d5c50b85f
maggemkay <magge.96@hotmail.com>
2020-05-12 21:07:38 +0200

    Adds full functionality of creating and saving a meeting with participants.

--- app/src/main/java/com/example/aristotle/MainActivity/MeetingFragment.kt
+++ app/src/main/java/com/example/aristotle/MainActivity/MeetingFragment.kt
@@ -1,30 +1,48 @@
 package com.example.aristotle
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.aristotle.MainActivity.MainActivity
+import com.example.aristotle.Models.Meeting
+import com.example.aristotle.Models.User
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.fragment_meeting.*
+import java.io.*
+import java.lang.reflect.Type
 import java.util.*
+
 
 class MeetingFragment : Fragment() {
 
+    private lateinit var pathToMeetingsFile : String
+    private lateinit var pathToUserFile : String
+
     private var startTimeSelected = Calendar.getInstance()
     private var endTimeSelected = Calendar.getInstance()
-
     private lateinit var currentSelected : Calendar
+
+    private lateinit var participantChipGroup : ChipGroup
+    private var clickedParticipants = mutableListOf<User>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        pathToMeetingsFile = this.context?.filesDir?.path + "/Meetings.json"
+        pathToUserFile = this.context?.filesDir?.path + "/Users.json"
+
         return inflater.inflate(R.layout.fragment_meeting, container, false)
     }
 
@@ -34,6 +52,26 @@ class MeetingFragment : Fragment() {
         timePicker.setIs24HourView(true)
         calendarViewSet.showCurrentMonthPage()
 
+        participantChipGroup = participantsChipGroupView
+
+        val users = loadUsers()
+
+        for (user: User in users) {
+            val chip = Chip(participantChipGroup.context)
+            chip.setText(user.firstName + " " + user.lastName)
+            chip.isCheckable = true
+            participantChipGroup.addView(chip)
+
+            chip.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked)
+                    clickedParticipants.add(user)
+                else
+                    clickedParticipants.remove(user)
+            }
+
+        }
+
+
         startTime.setOnClickListener {
             val animation = AnimationUtils.loadAnimation(
                 activity, R.anim.slide_up)
@@ -42,6 +80,7 @@ class MeetingFragment : Fragment() {
             deactivateElements()
 
             currentSelected = startTimeSelected
+
         }
 
         endTime.setOnClickListener {
@@ -72,15 +111,13 @@ class MeetingFragment : Fragment() {
             setter.startAnimation(animation)
             setter.visibility = View.INVISIBLE
 
-            Toast.makeText(activity , "Selected time " + timePicker.hour.toString().padStart(2, '0') + ":" + timePicker.minute.toString().padStart(2, '0') , Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                activity ,
+                "Selected time " + timePicker.hour.toString().padStart(2, '0') + ":" + timePicker.minute.toString().padStart(2, '0') ,
+                Toast.LENGTH_SHORT
+            ).show()
             currentSelected.set(Calendar.HOUR, timePicker.hour)
             currentSelected.set(Calendar.MINUTE, timePicker.minute)
-
-            Log.d("Selected year", currentSelected.get(Calendar.YEAR).toString())
-            Log.d("Selected month", currentSelected.get(Calendar.MONTH).toString())
-            Log.d("Selected day", currentSelected.get(Calendar.DAY_OF_MONTH).toString())
-            Log.d("Selected hour", currentSelected.get(Calendar.HOUR_OF_DAY).toString())
-            Log.d("Selected minute", currentSelected.get(Calendar.MINUTE).toString())
 
             activateElements()
         }
@@ -93,7 +130,15 @@ class MeetingFragment : Fragment() {
         }
 
         buttonNewMeetingBook.setOnClickListener {
-            saveMeeting()
+            val newMeeting = Meeting(
+                startTimeSelected.time,
+                endTimeSelected.time,
+                editTextMeetingSubject.text.toString(),
+                editTextLocation.text.toString(),
+                clickedParticipants
+            )
+
+            saveMeeting(newMeeting)
 
             val builder = this.context?.let { it1 -> AlertDialog.Builder(it1) }
             builder?.setTitle("Your meeting is booked")
@@ -122,9 +167,50 @@ class MeetingFragment : Fragment() {
         textInputLocation.isEnabled = false
     }
 
-    private fun saveMeeting() {
-        // Save the meeting to local storage
+    private fun saveMeeting(newMeeting: Meeting) {
+        var meetingsJson = loadMeetings()
 
+        if (!meetingsJson.contains(newMeeting)) {
+            meetingsJson.add(newMeeting)
+        }
+        val writer: Writer = FileWriter(pathToMeetingsFile)
+        Gson().toJson(meetingsJson, writer)
+        writer.close()
+
+    }
+
+    private fun loadMeetings() : MutableList<Meeting> {
+        var meetingsJson = mutableListOf<Meeting>()
+
+        try {
+            val reader: Reader = FileReader(pathToMeetingsFile)
+            val REVIEW_TYPE : Type = object : TypeToken<List<Meeting?>?>() {}.type
+            meetingsJson = Gson().fromJson(reader, REVIEW_TYPE) // contains the whole reviews list
+            reader.close()
+        } catch (e : FileNotFoundException) {
+            val writer: Writer = FileWriter(pathToMeetingsFile)
+            Gson().toJson(meetingsJson, writer)
+            writer.close()
+        }
+
+        return meetingsJson
+    }
+
+    private fun loadUsers() : MutableList<User> {
+        var jsonUsers = mutableListOf<User>()
+
+        try {
+            val reader: Reader = FileReader(pathToUserFile)
+            val REVIEW_TYPE : Type = object : TypeToken<List<User?>?>() {}.type
+            jsonUsers = Gson().fromJson(reader, REVIEW_TYPE) // contains the whole reviews list
+            reader.close()
+        } catch (e : FileNotFoundException) {
+            val writer: Writer = FileWriter(pathToUserFile)
+            Gson().toJson(jsonUsers, writer)
+            writer.close()
+        }
+
+        return jsonUsers
     }
 
 
---
+++ app/src/main/java/com/example/aristotle/Models/Meeting.kt
@@ -0,0 +1,11 @@
+package com.example.aristotle.Models
+
+import java.util.*
+
+class Meeting(
+    val startTime :Date,
+    val endTime :Date,
+    val subject :String,
+    val location :String,
+    val participants :List<User>
+)
